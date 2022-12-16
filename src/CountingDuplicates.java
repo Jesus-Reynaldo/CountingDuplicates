7cf5b2d98d1b0b6e77651834f848fb3eb598a073
Jesus Reynaldo <jezuz.of.murder@gmail.com>
2022-12-16 15:33:07 -0400

    Count the number of Duplicates

--- src/CountingDuplicates.java
+++ src/CountingDuplicates.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class CountingDuplicates {
+
+public class CountingDuplicates {
+    public static void main(String[] args) {
+        System.out.println(duplicateCount("aaabbmascclij"));
+    }
+    public static int duplicateCount(String text) {
+        // Write your code here
+        int count = 0;
+        String textLower=text.toLowerCase();
+        for (int i = 0; i < textLower.length(); i++) {
+            char caracterA = textLower.charAt(i);
+             for (int j = i+1; j < textLower.length(); j++) {
+                 char caracterB = textLower.charAt(j);
+                 if(caracterA==caracterB && caracterA!='_'){
+                     textLower=textLower.replace(caracterB,'_');
+                     count++;
+                 }
+             }
+        }
+        return count;
+    }
+
 }
